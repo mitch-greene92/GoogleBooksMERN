ab371af68b292c8e4fbce056e6d25455c446e12d
mitchgreene92 <mitchgreene92@gmail.com>
2020-06-16 18:17:02 -0500

    attempting to add build for deployment

---
+++ client/build/precache-manifest.343289caf0eae722f928901f7c141ed4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1924580e4947f7e66204d88baad301e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31253c425ba3ec5a5d45",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "f5f894463f9e39bee90c",
+    "url": "/static/css/main.765c34a1.chunk.css"
+  },
+  {
+    "revision": "31253c425ba3ec5a5d45",
+    "url": "/static/js/2.dd8f079d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.dd8f079d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5f894463f9e39bee90c",
+    "url": "/static/js/main.53d3feea.chunk.js"
+  },
+  {
+    "revision": "27e7b6d1095f4f8a0aa9",
+    "url": "/static/js/runtime-main.08c4e3d1.js"
+  }
+]);
